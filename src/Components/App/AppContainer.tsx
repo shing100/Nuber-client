1da90963640678be22a6f7a8b7bb10cd7a4bd9a5
limgeun <shing100@naver.com>
2019-04-12 13:01:38 +0900

    Typescript를 이용한 Componetns

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import AppPresneter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQueries";
 
 // gql 에서 일어날 변화의 대한 데이터
-const AppContainer = ({ data }) => <div>{JSON.stringify(data)}</div>;
+const AppContainer = ({ data }) => <AppPresneter isLoggedIn={data.auth.isLoggedIn} />
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
