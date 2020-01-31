fa0653ef16d1dfde4479e39b10ce4559e069630e
Lim Geun <shing100@naver.com>
2020-02-01 08:53:19 +0900

    VerifyPhone Mutation

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../../global-styles";
 import theme from "../../theme";
 import AppPresneter from "./AppPresenter";
-import { IS_LOGGED_IN } from "./AppQueries";
+import { IS_LOGGED_IN } from "./AppQueries.local";
 
 
 // gql 에서 일어날 변화의 대한 데이터
