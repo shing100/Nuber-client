3678e0988391c56c14f095267c39c3d253364465
Lim Geun <shing100@naver.com>
2020-02-07 23:46:33 +0900

    SocialLogin with Facebook

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -25,6 +25,31 @@ export interface startPhoneVerification_StartPhoneVerification {
   // This file was automatically generated and should not be edited.
 
   // ====================================================
+  // GraphQL mutation operation: facebookConnect
+  // ====================================================
+
+  export interface facebookConnect_FacebookConnect {
+    __typename: "FacebookConnectResponse";
+    ok: boolean;
+    error: string | null;
+    token: string | null;
+  }
+
+  export interface facebookConnect {
+    FacebookConnect: facebookConnect_FacebookConnect;
+  }
+
+  export interface facebookConnectVariables {
+    firstName: string;
+    lastName: string;
+    email?: string | null;
+    fbId: string;
+}
+
+  /* tslint:disable */
+  // This file was automatically generated and should not be edited.
+
+  // ====================================================
   // GraphQL mutation operation: verifyPhone
   // ====================================================
 
