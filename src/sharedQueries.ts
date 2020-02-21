da8e484f69d43dcdc941d6035688be6f528f0333
Lim Geun <shing100@naver.com>
2020-02-21 22:23:36 +0900

    Home sidebar Query

--- src/sharedQueries.ts
+++ src/sharedQueries.ts
@@ -1,7 +1,15 @@
 import { gql } from "apollo-boost";
 
-export const LOG_USER_IN = gql`
-  mutation logUserIn($token: String!) {
-    logUserIn(token: $token) @client
+export const USER_PROFILE = gql`
+  query userProfile {
+    GetMyProfile {
+      ok
+      error
+      user {
+        profilePhoto
+        fullName
+        isDriving
+      }
+    }
   }
 `;
